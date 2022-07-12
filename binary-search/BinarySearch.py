3b4084f66c74b1e162ef8b2829e6ff510b899d4b
Mohamed Gad <mohamedgad149@gmail.com>
2022-07-12 12:16:23 +0200

    binary search implementation

---
+++ binary-search/BinarySearch.py
@@ -0,0 +1,23 @@
+import math
+
+
+class BinarySearch:
+    def __init__(self, list):
+        self.list = list
+
+    def binarySearch(self, element, start, end):
+        middle = math.floor((start + end) / 2)
+        if (self.list[middle] == element):
+            return middle
+        if (self.list[middle] >= element):
+            return self.binarySearch(element, start, middle - 1)
+
+        if (self.list[middle] <= element):
+            return self.binarySearch(element, middle + 1, end)
+
+    def findIndex(self, element):
+        end = len(self.list)
+        return self.binarySearch(element, 0, end)
+
+    def sort(self):
+        self.list.sort()
---
+++ binary-search/client.py
@@ -0,0 +1,11 @@
+from BinarySearch import BinarySearch
+
+nums = [1, 5, 2, 3, 10, 50]
+binarySearch = BinarySearch(nums)
+binarySearch.sort()
+
+# nums is now [1 , 2 , 3 , 5 , 10 , 50]
+# 5 has index of 3
+indexOfFive = binarySearch.findIndex(5)
+
+print(f'found 5 at index {indexOfFive}')
